fe93a33134f008858a51dc8d630ba05e33b3532d
David Mobrand <david.mobrand@majority.com>
2025-04-06 02:54:00 +0200

    ci: rename Jest config files to use .cjs extension

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
